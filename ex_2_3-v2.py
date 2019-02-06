f7f1b0d77559ef50b6e7fb586ce488ff407395f2
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-06 20:51:21 +0100

    Update ex_2_3-v2.py

--- ex_2_3-v2.py
+++ ex_2_3-v2.py
@@ -7,14 +7,13 @@ with open("kodeData.dat", "r") as f:
     kodeDataLists = [(line.split(" ")) for line in f]
 
 nameCounter = 1
-for i in range(1, len(kodeDataStrings)-1):
+for i in range(1, len(kodeDataStrings)):
     if len(kodeDataLists[i]) == 10:
         print ("Daten aus Iterations-Block; kodeData Zeile:", str(i), "- Schreibe in \"kode-test%s.dat" % (nameCounter))
         with open("kode%s.dat" % (nameCounter), "a") as f:
             f.writelines(kodeDataStrings[i])
     elif len(kodeDataLists[i][0]) != len(kodeDataLists[i-1][0]) and len(kodeDataLists[i-1][0]) == len(kodeDataLists[i-2][0]):
-        print ("Ende Iterations-Block; kodeData Zeile:", str(i))
+        print ("Ende Iterations-Block %s; kodeData Zeile:" % (nameCounter), str(i))
         nameCounter += 1
     else:
         print ("nix los hier, Zeile:", str(i), "- Inhalt:", kodeDataStrings[i])
-
