7c089b3b4803acc8e43296ae654d817f03373bdf
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-06 21:59:55 +0100

    Update ex_2_3-v2.py

--- ex_2_3-v2.py
+++ ex_2_3-v2.py
@@ -4,18 +4,16 @@ with open("kodeData.dat", "r") as f:
     kodeDataStrings = [line for line in f]
 
 with open("kodeData.dat", "r") as f:
-    kodeDataLists = [(line.split(" ")) for line in f]
+    kodeDataLists = [(line.split()) for line in f]
 
 nameCounter = 1
 for i in range(1, len(kodeDataStrings)):
     if len(kodeDataLists[i]) == 10:
-        print ("Daten aus Iterations-Block %s; kodeData Zeile:" % (nameCounter), str(i), "- Schreibe in \"kode%s.dat" % (nameCounter))
+        print ("kodeData Zeile:", str(i), "| Daten aus Iterations-Block %s - Schreibe in kode%s.dat" % (nameCounter,nameCounter))
         with open("kode%s.dat" % (nameCounter), "a") as f:
             f.writelines(kodeDataStrings[i])
-    elif len(kodeDataLists[i][0]) != len(kodeDataLists[i-1][0]) and len(kodeDataLists[i-1][0]) == len(kodeDataLists[i-2][0]):
-        print ("Ende Iterations-Block %s; kodeData Zeile:" % (nameCounter), str(i))
+    elif len(kodeDataLists[i]) != len(kodeDataLists[i-1]) and len(kodeDataLists[i-1]) == len(kodeDataLists[i-2]):
+        print ("kodeData Zeile:", str(i), "|- Ende von Iterations-Block", str(nameCounter), "- in Zeile", str(i-1))
         nameCounter += 1
     else:
-        print ("nix los hier, Zeile:", str(i), "- Inhalt:", kodeDataStrings[i])
-
-
+        print ("kodeData Zeile:", str(i), "| Inhalt:", kodeDataStrings[i])
