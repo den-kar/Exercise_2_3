4970d6b00b8e59cfa1d818b54b4897aec60be218
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-07 12:13:04 +0100

    Update ex_2_3.py

--- ex_2_3.py
+++ ex_2_3.py
@@ -1,16 +1,16 @@
 #! /usr/bin/python3.6
 
-#Get data from kodeData.dat
+# Get data from kodeData.dat
 with open("kodeData.dat", "r") as f:
     kodeData = [line.split() for line in f] 
 
-#Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichende)
-iterBlockStartRow = [i for i in range(1, len(kodeData)) if\
-                     len(kodeData[i]) == int(kodeData[1][0]) and len(kodeData[i-1]) == int(kodeData[2][0])]
+# Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichende)
+itDataStartRow = [i for i in range(1, len(kodeData)) if\
+len(kodeData[i]) == int(kodeData[1][0]) and len(kodeData[i-1]) == int(kodeData[2][0])]
 
-#write data as strings (Format: ". . ."\n, ". . "\n, ...) 
+# Write data to "kodeD#.dat" (Format: ". . ."\n, ". . "\n, ...) 
 for i in range(int(kodeData[1][1])):
     with open("kodeD%s.dat" % (i+1), "w") as f:
-        f.writelines(line for line in ([" ".join(line) + "\n" for line in kodeData]\
-                                       [int(iterBlockStartRow[i]):(int(iterBlockStartRow[i])+int(kodeData[1][0]))]))
+        f.writelines(line for line in\
+([" ".join(line) + "\n" for line in kodeData][int(itDataStartRow[i]):(int(itDataStartRow[i])+int(kodeData[1][0]))]))
 
