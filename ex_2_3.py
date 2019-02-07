9ee8801504cfd10e0cc617107b5838e0328f9d28
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-07 12:28:55 +0100

    Update ex_2_3.py

--- ex_2_3.py
+++ ex_2_3.py
@@ -4,13 +4,13 @@
 with open("kodeData.dat", "r") as f:
     kodeData = [line.split() for line in f] 
 
-# Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichende)
-itDataStartRow = [i for i in range(1, len(kodeData)) if\
-len(kodeData[i]) == int(kodeData[1][0]) and len(kodeData[i-1]) == int(kodeData[2][0])]
+# Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichend)
+itDataStartRow = [lineNumber for lineNumber in range(1, len(kodeData)) if \
+len(kodeData[lineNumber]) == int(kodeData[1][0]) and len(kodeData[lineNumber-1]) == int(kodeData[2][0])]
 
 # Write data to "kodeD#.dat" (Format: ". . ."\n, ". . "\n, ...) 
 for i in range(int(kodeData[1][1])):
     with open("kodeD%s.dat" % (i+1), "w") as f:
-        f.writelines(line for line in\
-([" ".join(line) + "\n" for line in kodeData][int(itDataStartRow[i]):(int(itDataStartRow[i])+int(kodeData[1][0]))]))
+        f.writelines(line for line in (\
+[" ".join(line) + "\n" for line in kodeData][int(itDataStartRow[i]):(int(itDataStartRow[i])+int(kodeData[1][0]))]))
 
