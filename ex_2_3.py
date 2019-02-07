1a30a4a4589ce7abc9c0b26119a4298935bbad7a
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-07 14:24:30 +0100

    Update ex_2_3.py

--- ex_2_3.py
+++ ex_2_3.py
@@ -2,15 +2,17 @@
 
 # Get data from kodeData.dat
 with open("kodeData.dat", "r") as f:
-    kodeData = [line.split() for line in f] 
+    kodeData = [line.split() for line in f]
 
 # Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichend)
 itDataStartRow = [lineNumber for lineNumber in range(1, len(kodeData)) if \
 len(kodeData[lineNumber]) == int(kodeData[1][0]) and len(kodeData[lineNumber-1]) == int(kodeData[2][0])]
 
-# Write data to "kodeD#.dat" (Format: ". . ."\n, ". . "\n, ...) 
+# Write data to "kodeD#.dat" (Format: ". . ."\n, ". . "\n, ...)
 for i in range(int(kodeData[1][1])):
     with open("kodeD%s.dat" % (i+1), "w") as f:
         f.writelines(line for line in (\
 [" ".join(line) + "\n" for line in kodeData][int(itDataStartRow[i]):(int(itDataStartRow[i])+int(kodeData[1][0]))]))
+    print ("saved in kodeD%s.dat|" % (i+1), kodeData[1][0], "iterations", "- step size:", kodeData[itDataStartRow[i]-1])
 
+ 
