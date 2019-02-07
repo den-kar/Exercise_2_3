85aa3d9b548552ea5bc65e44c6991682952ff2e8
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-07 11:24:59 +0100

    Update ex_2_3.py

--- ex_2_3.py
+++ ex_2_3.py
@@ -6,13 +6,7 @@ with open("kodeData.dat", "r") as f:
 #Get start rows of iteration-blocks (bissi random, aber hinreichende Bedingung)
 iterBlockStartRow = [i for i in range(1, len(kodeData)) if (len(kodeData[i]) == int(kodeData[1][0]) and len(kodeData[i-1]) == 1)]
 
-#write data as lists (Format: "['x','y',...]"\n ['...',...])
-for i in range(int(kodeData[1][1])):
-    with open("kodeZLists%s.dat" % (i+1), "w") as f:
-        f.writelines(str(line) + "\n" for line in (kodeData[int(iterBlockStartRow[i]):(int(iterBlockStartRow[i])+10)]))
-# + "\n" für Formatierung
-
 #write data as strings (Format: "[x y z ...]"\n [. . ...]) 
 for i in range(int(kodeData[1][1])):
-    with open("kodeZStrings%s.dat" % (i+1), "w") as f:
+    with open("kodeD%s.dat" % (i+1), "w") as f:
         f.writelines(line for line in ([" ".join(line) + "\n" for line in kodeData][int(iterBlockStartRow[i]):(int(iterBlockStartRow[i])+10)]))
