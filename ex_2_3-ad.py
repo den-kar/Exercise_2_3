982a599a1e0523a745056d4839c6df9b83431dc6
den-kar <47293315+den-kar@users.noreply.github.com>
2019-02-15 19:04:07 +0100

    Update ex_2_3-ad.py

--- ex_2_3-ad.py
+++ ex_2_3-ad.py
@@ -4,22 +4,25 @@
 with open("kodeData.dat", "r") as f:
     kodeData = [line.split() for line in f]
 
+iterBlockLineLength = int(kodeData[1][0])
+iterStepLineLength = int(kodeData[2][0])
+
 # Get start rows of iteration-blocks (Bedingung bissi random gewählt, aber hinreichend)
 itDataStartRow = [lineNumber for lineNumber in range(1, len(kodeData)) if \
-len(kodeData[lineNumber]) == int(kodeData[1][0]) and len(kodeData[lineNumber-1]) == int(kodeData[2][0])]
+len(kodeData[lineNumber]) == iterBlockLineLength and len(kodeData[lineNumber-1]) == iterStepLineLength]
 
 
 """ --- Exercise 2.3 Advanced --- """
 
 # invoke empty list for elements of 10x10 iteration blocks
-kodeDataSums = [[0 for row in range(int(kodeData[1][0]))] for col in range(int(kodeData[1][0]))]
+kodeDataSums = [[0 for row in range(iterBlockLineLength)] for col in range(iterBlockLineLength)]
 xSum = 0
 
 # fill list with summarized values from iteration blocks
 for i in range(len(itDataStartRow)):
     xSum += float(kodeData[itDataStartRow[i]-1][0])
-    for j in range(int(kodeData[1][0])):
-        for k in range(int(kodeData[1][0])):
+    for j in range(iterBlockLineLength):
+        for k in range(iterBlockLineLength):
             kodeDataSums[j][k] += float(kodeData[itDataStartRow[i]+j][k])
 
 # write sums to kodeData-Sums.dat
